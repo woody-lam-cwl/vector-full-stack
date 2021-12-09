df49a7940535ef90e462fed9efa1ebe263dc1438
Woody Lam <woodylam@pc-21-147-5.magd.private.cam.ac.uk>
2021-12-09 01:23:44 +0000

    Add basic react framework

---
+++ full-stack-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Card from './card';
+
+function App() {
+  return (
+    <div>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ full-stack-app/src/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Card extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Card</h1>
+                <img src='' alt=''/>
+            </div>
+        );
+    }
+}
+
+export default Card;
